082fdd9a239b56d653da758a13364a65d33b6ec7
Ignacio De Nigris <denigris.ignacioj@gmail.com>
2024-12-20 22:54:34 -0300

    agregando modelos y maneras de ingresar datos

--- AppClub/models.py
+++ AppClub/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+class Inscripcion(models.Model):
+    nombre = models.CharField(max_length=100) 
+    apellido = models.CharField(max_length=30)
+    deporte = models.CharField(max_length=30)
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=30) 
+    apellido = models.CharField(max_length=30) 
+    email = models.EmailField() 
+    profesion = models.CharField(max_length=50)  
+
+class Copetencias(models.Model):
+    nombre = models.CharField(max_length=100)
+    deporte = models.CharField(max_length=30)
+    fechaDeCopetencia = models.DateField()  
+    entregado = models.BooleanField() 
+
 # Create your models here.
